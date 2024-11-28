1c267c59195be4a0d4183464658147426e9ef926
Abraham Ortega <AbrahamOrtegaM@outlook.com>
2024-11-28 14:11:22 -0600

    add readme

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
+      <title>Rest Countries API</title>
       <Head />
       <body className="antialiased bg-veryLightGray dark:bg-veryDarkBlue">
         <Main />
